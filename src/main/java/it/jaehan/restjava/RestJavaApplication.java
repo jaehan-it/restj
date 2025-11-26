39973a8442b3874d17b16c081a9d18783c5ae999
PlusPro_Jaehan <jaehan.kim@pluspro.kr>
2025-11-26 11:07:25 +0900

    Test Commit

--- src/main/java/it/jaehan/restjava/RestJavaApplication.java
+++ src/main/java/it/jaehan/restjava/RestJavaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestJavaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RestJavaApplication.class, args);
+        System.out.println("Hello");
     }
 
 }
